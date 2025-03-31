072ba1ea0d3cedc89069e50824ae8a21a77d0718
M. Safarian <ma.hiccup@gmail.com>
2025-04-01 00:16:41 +0330

    fix

--- ProjectEuler/PU4.c
+++ ProjectEuler/PU4.c
@@ -1,11 +1,10 @@
 /**
- * @file        template.c
+ * @file        PU4.c
  * @author      Mehdi Safarian(MayT)
- * @date        2025-03-31
+ * @date        2025-04-01
  * @version     1.0
- * @brief       ...
  *
- * @details    details
+ * @details    Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
  */
 
  #include <stdio.h>
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # My-C-Practices
+Here you find my practice in c.
